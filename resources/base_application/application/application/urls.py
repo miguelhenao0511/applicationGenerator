f503f94b593f3e5b0e959d1b36f9937a2e14c489
Miguel Henao <mhenaoh@unal.edu.co>
2020-11-25 15:23:25 -0500

    Add index page

--- resources/base_application/application/application/urls.py
+++ resources/base_application/application/application/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
 #@application_urls_imports
 
+def index(request):
+    return render(request, 'index.html')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 
     #@application_urls_patterns_path
 ]
